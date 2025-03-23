0454c135bc603dcdcc90dc38610deaea3c306e82
prashantlord <prashants2062@gmail.com>
2025-03-23 17:34:57 +0545

    No further development

---
+++ JS/index.js
@@ -0,0 +1,17 @@
+let Eyoutube_video1 = document.getElementById("youtube_video1");
+let Eyoutube_video2 = document.getElementById("youtube_video2");
+let Eyoutube_video3 = document.getElementById("youtube_video3");
+let Eyoutube_video4 = document.getElementById("youtube_video4");
+
+Eyoutube_video1.onclick = () => {
+    window.location.href = 'https://www.youtube.com/watch?v=89KaaY_vVvo';
+}
+Eyoutube_video2.onclick = () => {
+        window.location.href = "https://www.youtube.com/watch?v=QxUcmf8PI4o";
+}
+Eyoutube_video3.onclick = () => {
+    window.location.href = "https://www.youtube.com/watch?v=dQfaRGer3Rk";
+}
+Eyoutube_video4.onclick = () => {
+    window.location.href = "https://www.youtube.com/watch?v=cC9TS8cBxFI&t=9365s";
+}
